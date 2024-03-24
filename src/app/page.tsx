e1d472e85827a4d057895901640c9a7ee4468523
Kurniawan <53652218+kurnhyalcantara@users.noreply.github.com>
2024-03-24 19:40:55 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { AspectRatio, Center, Stack, Heading } from '@chakra-ui/react';
+import Image from 'next/image';
+
+const Home = () => {
+  return (
+    <Center h="100vh">
+      <Stack align="center" spacing="8">
+        <AspectRatio ratio={283 / 64} w="200px">
+          <Image
+            src="/vercel.svg"
+            alt="vercel"
+            fill
+            sizes="(max-width: 768px) 100vw,
+          (max-width: 1200px) 50vw,
+          33vw"
+          />
+        </AspectRatio>
+
+        <Heading size="lg">NextJS-Typescript-Chakra-UI-Template</Heading>
+      </Stack>
+    </Center>
+  );
+};
+
+export default Home;
---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {},
+  config: {},
+  shadows: {},
+  colors: {},
+  components: {},
+});
+
+export default theme;
